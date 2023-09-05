44f102f42ac048466b9804ce82ff774a26ca6f85
yudiell.hernandez <yudiell.hernandez@loves.com>
2023-09-05 09:48:30 -0500

    Setup for the use of a config file and model choices.

--- pypostman/utils/cli.py
+++ pypostman/utils/cli.py
@@ -7,7 +7,7 @@ class Cli:
 
     @property
     def envs(self):
-        envs: list = ["temp", "dev", "prod"]
+        envs: list = ["temp", "dev", "prod", "qa"]
         return envs
 
     def parse_arguments(self):
@@ -28,13 +28,33 @@ class Cli:
             required=False,
             help="Tag that determines which tagged flow runs, select from the flows corresponding model flow.",
         )
+
+        parser.add_argument(
+            "--select",
+            metavar="SELECT",
+            type=str,
+            nargs="*",
+            required=False,
+            help="Select allows you to select multiple tags, ex: --select tag:tag1 tag:tag2 tag:tag3",
+            default=[],
+        )
+
+        parser.add_argument(
+            "--args",
+            metavar="ARGS",
+            type=str,
+            nargs="*",
+            required=False,
+            help="Additional arguments to pass to the flow, ex: --args arg1 arg2 arg3",
+            default=[],
+        )
+
         parser.add_argument(
             "-r",
             "--refresh",
             type=str,
             required=False,
-            help="",
-            choices=["full"],
+            help="Add a date to refresh the data.",
         )
         # get args
         args = parser.parse_args()
